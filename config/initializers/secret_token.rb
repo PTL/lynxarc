785b3d35de44c65164b1cddc0ef87155ddc4a17c
balakrishna <balakrishna.karki@parakhtechnologies.com>
2011-03-04 12:16:00 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lynxarc::Application.config.secret_token = '0549b4d5f74f620df917ac2ad214119e47f87aea918b612ab630ef50b15ade2b9fd2cee7026ef1fbc822702fe0e63406aa5c42298f1c487c7f3c178e240e5d91'
